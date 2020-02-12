64bd9f43f65d52482b03636e470d641f403af10c
Ibrahim Ozdemir <53236678+ibrahimcahit@users.noreply.github.com>
2020-02-12 13:34:41 +0300

    Add files via upload

---
+++ Alexa_Skill_Template_for_Lambda.py
@@ -0,0 +1,121 @@
+"""
+This is a Python template for Alexa to get you building skills quickly.
+
+
+https://github.com/ibrahimcahit
+"""
+
+from __future__ import print_function
+import json
+
+def build_speechlet_response(title, output, reprompt_text, should_end_session):
+    return {
+        'outputSpeech': {
+            'type': 'PlainText',
+            'text': output
+        },
+        'card': {
+            'type': 'Simple',
+            'title': "SessionSpeechlet - " + title,
+            'content': "SessionSpeechlet - " + output
+        },
+        'reprompt': {
+            'outputSpeech': {
+                'type': 'PlainText',
+                'text': reprompt_text
+            }
+        },
+        'shouldEndSession': should_end_session
+    }
+
+def build_response(session_attributes, speechlet_response):
+    return {
+        'version': '1.0',
+        'sessionAttributes': session_attributes,
+        'response': speechlet_response
+    }
+
+
+def get_test_response():
+    session_attributes = {}
+    card_title = "Relay"
+    speech_output = "Switch one is opened!"
+    reprompt_text = "You never responded to the first test message. Sending another one."
+    should_end_session = False
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+        
+def get_introduce():
+    
+    session_attributes = {}
+    card_title = "introduce"
+    speech_output = "You are now running Smart Home system; version 0.1.7 . Smart Home system is created by Ibrahim Ozdemir. It's Node Red and AWS Lambda based, serverless IoT application. "
+    reprompt_text = "You never responded to the first test message. Sending another one."
+    should_end_session = False
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+def get_welcome_response():
+    session_attributes = {}
+    card_title = "Welcome"
+    
+    speech_output = "Smart home system is activated. Welcome to the Smart Home System! "
+    reprompt_text = "I don't know if you heard me, try to say something to run system!"
+    should_end_session = False
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+
+def handle_session_end_request():
+    card_title = "Session Ended"
+    speech_output = "Smart Home system is deactivated. Have a nice day! "
+    
+    should_end_session = True
+    return build_response({}, build_speechlet_response(
+        card_title, speech_output, None, should_end_session))
+
+
+
+def on_session_started(session_started_request, session):
+    pass
+
+    
+
+def on_launch(launch_request, session):
+    return get_welcome_response()
+
+
+def on_intent(intent_request, session):
+    intent = intent_request['intent']
+    intent_name = intent_request['intent']['name']
+
+    if intent_name == "Relay":
+        return get_test_response()
+    elif intent_name == "AMAZON.HelpIntent":
+        return get_welcome_response()
+    elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
+        return handle_session_end_request()
+    elif intent_name == "introduce":
+        return get_introduce()
+    else:
+        raise ValueError("Invalid intent")
+    
+
+
+def on_session_ended(session_ended_request, session):
+    print("on_session_ended requestId=" + session_ended_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+
+def lambda_handler(event, context):
+    print("Incoming request...")
+
+    if event['session']['new']:
+        on_session_started({'requestId': event['request']['requestId']},
+                           event['session'])
+
+    if event['request']['type'] == "LaunchRequest":
+        return on_launch(event['request'], event['session'])
+    elif event['request']['type'] == "IntentRequest":
+        return on_intent(event['request'], event['session'])
+    elif event['request']['type'] == "SessionEndedRequest":
+        return on_session_ended(event['request'], event['session'])
